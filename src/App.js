71ff308ad08f2b2a82ad30c0e5f920aa5f92afb4
David Godfrey <david.godfrey99@gmail.com>
2022-06-08 23:52:53 -0400

    Update project ID for ChatEngine

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '13e8978c-5d77-45ac-99a7-c1815f6f1696';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '13e8978c-5d77-45ac-99a7-c1815f6f1696';
 
 const Modal = () => {
   const [username, setUsername] = useState('');
